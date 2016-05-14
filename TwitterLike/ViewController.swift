576d49f684607bdab274f24b3041cdd1527de782
adin bautista <adinbautista@gmail.com>
2016-05-14 14:12:36 +0800

    Initial Commit

---
+++ TwitterLike/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TwitterLike
+//
+//  Created by Aldrin Bautista on 14/05/2016.
+//  Copyright © 2016 Aldrin Bautista. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
